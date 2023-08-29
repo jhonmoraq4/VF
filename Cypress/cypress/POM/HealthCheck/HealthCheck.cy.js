ebd8679c45d2ff756f3ba6fbe27eb7c3544ca6ba
jdnavia <navia_1031@hotmail.com>
2023-08-29 17:48:59 -0500

    failcucumber

--- Cypress/cypress/POM/HealthCheck/HealthCheck.cy.js
+++ Cypress/cypress/POM/HealthCheck/HealthCheck.cy.js
@@ -5,15 +5,11 @@ class HealthCheckPage {
   
     TakeYourFirstHealthCheck() {
       cy.get('#health-check-start-survey').click();
-      cy.wait(3000)
     }
   
     NextButtonClick() {
       cy.get('button[vf-button][preset-next]').click();
-    }
 
-    NextButtonThoughts() {
-      cy.get('button[id="health-check-go-to-thoughts-step"]').click();
     }
   
     BackButtonClick() {
@@ -29,33 +25,74 @@ class HealthCheckPage {
     }
   
     movitesScreen() {
-      cy.get('input[type="range"]').each(($slider) => {
-        const randomValue = Math.floor(Math.random() * 99) + 2; // Genera un valor aleatorio entre 2 y 100
-        cy.wrap($slider).invoke('val', randomValue).trigger('input');
-      })
-    }
-
-    thoughtsScreen() {
-      cy.get('div[role="textbox"]').type("Build a House")
-      cy.get('#health-check-go-to-habits-step').click()
-    }
-
-    habitsScreen() {
-      for (let i = 0; i < 10; i++) {
-        cy.get('.habit-item-wrapper').eq(i)  // Seleccionar la opción correspondiente
-          .find('.checkbox-item')             // Encontrar todos los elementos con la clase 'checkbox-item'
-          .eq(i === 0 ? 2 : 1)                // Seleccionar el tercer checkbox para la primera opción, y el segundo checkbox para las demás
-          .click();                           // Hacer clic en el checkbox correspondiente
+      const sliderHandle = '.mdc-slider__thumb';
+      
+      for (let i = 0; i < 6; i++) {
+        cy.get(sliderHandle)
+          .eq(i)
+          .then((slider) => {
+            const boundingBox = slider[0].getBoundingClientRect();
+            const start_x = boundingBox.x;
+            const start_y = boundingBox.y;
+  
+            cy.get(sliderHandle).eq(i).trigger('mousedown', { which: 1 });
+  
+            // Define how many pixels it will move
+            const pixels_to_drag = Cypress._.random(60, 200);
+  
+            // Calculate the new X_position
+            const end_x = start_x + pixels_to_drag;
+  
+            cy.get(sliderHandle)
+              .eq(i)
+              .trigger('mousemove', {
+                clientX: end_x,
+                clientY: start_y,
+              })
+              .trigger('mouseup', { force: true });
+  
+            if (i !== 0 && i % 2 === 0) {
+              cy.get(sliderHandle).eq(i).trigger('wheel', { deltaY: 200 });
+            }
+          });
+  
+        cy.wait(1000);
       }
-      cy.get('#health-check-go-to-ideas-step').click()
     }
-    
+  
+    thoughtsScreen() {
+      cy.get('application[name="Rich Text Editor"] paragraph').click();
+      cy.get('textbox[name="Editor editing area: main"]').type(
+        'I would like to know more about technology'
+      );
+    }
+  
+    habitsScreen() {
+      const maintainAsIsElements = '.mat-radio-container';
+      const improveLaterElements = '.mat-radio-container';
+      const prioritizeElements = '.mat-radio-container';
+  
+      for (let i = 0; i < 10; i += 3) {
+        cy.get(maintainAsIsElements).eq(i).click();
+        cy.wait(1000);
+      }
+  
+      for (let i = 11; i < 20; i += 2) {
+        cy.get(improveLaterElements).eq(i).click();
+        cy.wait(1000);
+      }
+  
+      for (let i = 21; i < 30; i += 3) {
+        cy.get(prioritizeElements).eq(i).click();
+        cy.wait(1000);
+      }
+    }
+  
     ideasScreen() {
       cy.get('textarea').each((textarea) => {
         cy.wrap(textarea).type('This is a test');
         cy.wait(1000);
       });
-      cy.get('#health-check-go-to-volunteers-step').click()
     }
   
     volunteersScreen() {
@@ -68,16 +105,6 @@ class HealthCheckPage {
   
     diagnosticSubmitted() {
       cy.get('button[mod*="accent"]').contains('Okay').click();
-    }
-
-    creatingReport() {
-      cy.get('#health-check-create-report').click();
-      cy.get('button[mat-flat-button][vfsteppernext][mod="accent"]').click();
-      cy.get('button[mat-flat-button][vfsteppernext][mod="accent"] span.mat-button-wrapper span:contains("That\'s correct")').click();
-      cy.get('#mat-checkbox-65').click();
-      cy.get('button[mat-flat-button][mod="accent"] span.mat-button-wrapper span:contains("Create report")').click();
-      cy.wait(2000)
-      cy.get('#health-check-creveal-step').click();
     }
   }
   
